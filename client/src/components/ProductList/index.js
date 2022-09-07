aba4c95ad693a2955c2e1b60a0209e9334f3b135
Samantha-Ruth <sam.haberman@gmail.com>
2022-09-07 15:38:53 -0600

    updated productList to use indexed DB when there is not internet connection

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
+import { idbPromise } from '../../utils/helpers'
 
 import ProductItem from '../ProductItem';
 import { useStoreContext } from '../../utils/GlobalState';
@@ -15,13 +16,27 @@ function ProductList() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
-    if (data) {
+    if(data) {
       dispatch({
         type: UPDATE_PRODUCTS,
-        products: data.products,
+        products: data.products
+      });
+  
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+      // add else if to check if `loading` is undefined in `useQuery()` Hook
+    } else if (!loading) {
+      // since we're offline, get all of the data from the `products` store
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
       });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
