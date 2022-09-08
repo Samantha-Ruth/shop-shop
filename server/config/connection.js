ec3b871d6ede202133317933cc7459a19c0d4de1
Samantha-Ruth <sam.haberman@gmail.com>
2022-09-08 12:35:57 -0600

    updated server connect.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/shop-shop', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
