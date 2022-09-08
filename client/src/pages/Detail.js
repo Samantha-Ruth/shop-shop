8c9b3d1f690e5ccbf94b414adcff3defb339cfca
Samantha-Ruth <sam.haberman@gmail.com>
2022-09-07 19:01:34 -0600

    added persistence to the cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -80,6 +80,8 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
+      // upon removal from cart, delete the item from IndexedDB using the `currentProduct._id` to locate what to remove
+  idbPromise('cart', 'delete', { ...currentProduct });
   };
 
   return (
